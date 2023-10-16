e21a64a6bcec8f8b337b06d301257faad544cc20
Rishi Raj <rishiraj007bgp@gmail.com>
2023-10-16 17:03:22 +0530

    add gameboard basic UI

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
+
 const gameboard = (function() {
-  const gb = ['X', 'O', 'X', 'O', 'O', 'X'];
+  const gb = [null, null, null, null, null, null];
   const gbContainer = document.querySelector('.container')
+  const position = document.querySelectorAll('.position');
+
+  
 
   function displayMarker() {
       for (let i = 0; i < gb.length; i++) {
@@ -9,22 +14,28 @@ const gameboard = (function() {
         gbContainer.appendChild(gbel)
       }
     }
-   return { displayMarker }
+   return { gb, displayMarker, position }
 })();
 
-gameboard.displayMarker()
+
 
 function Player(name, marker) {
+    let choice;
     const player = {}
     player.name = name;
     player.marker = marker;
     const showName = () => player.name = name;
     const showMarker = () => player.marker = marker;
-    return { showName, showMarker };
+    return { showName, showMarker, choice };
 }
 
 const player1 = Player('p1', 'X');
 const player2 = Player('p2', 'O');
 
-console.log(player1.showMarker())
 
+// player1.choice = 3
+
+// gameboard.gb[player1.choice] = player1.showMarker()
+// gameboard.displayMarker()
+
+// player1.choice = 3 -> set player1.showMarker to 3rd position in array.
